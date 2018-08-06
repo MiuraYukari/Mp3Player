f1ba155cc5372f7976ad88a071d4eb2b2c03440d
Yukari Miura <miura@ppfpart.co.jp>
2018-08-06 22:49:57 +0900

    第2回PPFチャレンジ

---
+++ Mp3Player/Mp3Player.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Mp3Player
+{
+    partial class Mp3Player
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelMp3File = new System.Windows.Forms.Label();
+            this.textBoxFileName = new System.Windows.Forms.TextBox();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonDialog = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxTitle = new System.Windows.Forms.TextBox();
+            this.textBoxArtist = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelMp3File
+            // 
+            this.labelMp3File.AutoSize = true;
+            this.labelMp3File.Location = new System.Drawing.Point(77, 41);
+            this.labelMp3File.Name = "labelMp3File";
+            this.labelMp3File.Size = new System.Drawing.Size(90, 18);
+            this.labelMp3File.TabIndex = 0;
+            this.labelMp3File.Text = "mp3ファイル";
+            // 
+            // textBoxFileName
+            // 
+            this.textBoxFileName.Location = new System.Drawing.Point(80, 95);
+            this.textBoxFileName.Name = "textBoxFileName";
+            this.textBoxFileName.Size = new System.Drawing.Size(444, 25);
+            this.textBoxFileName.TabIndex = 1;
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(446, 320);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(117, 60);
+            this.buttonStop.TabIndex = 3;
+            this.buttonStop.Text = "停止";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            // 
+            // buttonDialog
+            // 
+            this.buttonDialog.Location = new System.Drawing.Point(530, 94);
+            this.buttonDialog.Name = "buttonDialog";
+            this.buttonDialog.Size = new System.Drawing.Size(33, 26);
+            this.buttonDialog.TabIndex = 4;
+            this.buttonDialog.Text = "...";
+            this.buttonDialog.UseVisualStyleBackColor = true;
+            this.buttonDialog.Click += new System.EventHandler(this.buttonDialog_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 162);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 18);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "タイトル";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(77, 216);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 18);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "アーティスト";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(77, 268);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 18);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "アルバム";
+            // 
+            // textBoxTitle
+            // 
+            this.textBoxTitle.Location = new System.Drawing.Point(197, 162);
+            this.textBoxTitle.Name = "textBoxTitle";
+            this.textBoxTitle.Size = new System.Drawing.Size(366, 25);
+            this.textBoxTitle.TabIndex = 11;
+            // 
+            // textBoxArtist
+            // 
+            this.textBoxArtist.Location = new System.Drawing.Point(197, 216);
+            this.textBoxArtist.Name = "textBoxArtist";
+            this.textBoxArtist.Size = new System.Drawing.Size(366, 25);
+            this.textBoxArtist.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(197, 268);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(366, 25);
+            this.textBox4.TabIndex = 10;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Location = new System.Drawing.Point(80, 320);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(117, 60);
+            this.buttonPlay.TabIndex = 2;
+            this.buttonPlay.Text = "再生";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            // 
+            // Mp3Player
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(728, 450);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBoxArtist);
+            this.Controls.Add(this.textBoxTitle);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonDialog);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonPlay);
+            this.Controls.Add(this.textBoxFileName);
+            this.Controls.Add(this.labelMp3File);
+            this.Name = "Mp3Player";
+            this.Text = "mp3プレーヤー";
+            this.Load += new System.EventHandler(this.Mp3Player_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelMp3File;
+        private System.Windows.Forms.TextBox textBoxFileName;
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonDialog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxTitle;
+        private System.Windows.Forms.TextBox textBoxArtist;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Mp3Player/Mp3Player.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shell32;
+using System.Windows.Forms;
+
+
+namespace Mp3Player
+{
+    public partial class Mp3Player : Form
+    {
+        public Mp3Player()
+        {
+            InitializeComponent();
+        }
+
+        private void Mp3Player_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// ファイル選択ダイアログボタン
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonDialog_Click(object sender, EventArgs e)
+        {
+            //ダイアログボックスを表示
+            DialogResult dialog = openFileDialog1.ShowDialog();
+            //ダイアログボックスでOKが選択された場合、ダイアログボックスに選択された名前を表示
+            if(dialog == System.Windows.Forms.DialogResult.OK)
+            {
+                textBoxFileName.Text = openFileDialog1.FileName;
+            }
+        }
+
+
+
+        class ShowID3Tags
+        {
+            static void Tags()
+            {
+                ShellClass shell = new ShellClass();
+
+                string dir = @"C:\mp3"; // MP3ファイルのあるディレクトリ
+                string file = "";
+
+                Folder f = shell.NameSpace(dir);
+                FolderItem item = f.ParseName(file);
+            }
+        }
+
+    }
+}
